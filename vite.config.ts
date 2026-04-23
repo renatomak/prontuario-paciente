5b4963937cb83d5c3b01adc77037f666f09c09fe
Renato Marques da Silva <renato.mark.silva@gmail.com>
2026-04-23 12:49:46 -0300

    feat: config para criar o executavel

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  // ADICIONE ESTA LINHA ABAIXO
+  base: "./", 
+  
   server: {
     host: "::",
-      port: 8080,
+    port: 8080,
     hmr: {
       overlay: false,
     },
